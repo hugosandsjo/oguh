f3c213237d061544e2ff3601299dc7c31f397172
hugosandsjo <hugosandsjo@gmail.com>
2025-03-02 20:38:27 +0100

    removed searchparams

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -11,7 +11,7 @@ type Props = {
 };
 
 export async function generateMetadata(
-  { params, searchParams }: Props,
+  { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   // read route params
